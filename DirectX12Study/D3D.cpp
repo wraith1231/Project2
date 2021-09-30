53baeec7ec6848928a9312a37883f6e631639058
wraith12 <wraith12@naver.com>
2021-09-30 17:29:38 +0900

    빌드 되는지는 아직 체크 안해봤음... 너무 많다

--- DirectX12Study/D3D.cpp
+++ DirectX12Study/D3D.cpp
@@ -1,5 +1,7 @@
 #include "stdafx.h"
 #include "D3D.h"
+#include "SingletonManager.h"
+#include "Window.h"
 
 D3D::D3D()
 {
@@ -19,8 +21,14 @@ void D3D::Init()
 		_debugController->EnableDebugLayer();
 	}
 #endif
+	_backBufferFormat = DXGI_FORMAT_R8G8B8A8_UNORM;
+	_depthStencilFormat = DXGI_FORMAT_D24_UNORM_S8_UINT;
+	_currentBackBuffer = 0;
 
 	CreateD3DDevice();
+	CreateFence();
+	CreateCommand();
+
 	LogAdapters();
 	
 }
@@ -32,18 +40,25 @@ void D3D::Update()
 
 void D3D::Delete()
 {
+	_commandAllocator->Release();
+	_commandQueue->Release();
+	_commandList->Release();
+
+	_fence->Release();
 	_debugController->Release();
-	_mdxgiFactory->Release();
-	_pWarpAdapter->Release();
-	_md3dDevice->Release();
+	_dxgiFactory->Release();
+	_warpAdapter->Release();
+	_d3dDevice->Release();
 }
+
+#pragma region LOG
 
 void D3D::LogAdapters()
 {
 	UINT i = 0;
 	IDXGIAdapter* adapter = nullptr;
 	vector<IDXGIAdapter*> adapterList;
-	while (_mdxgiFactory->EnumAdapters(i, &adapter) != DXGI_ERROR_NOT_FOUND)
+	while (_dxgiFactory->EnumAdapters(i, &adapter) != DXGI_ERROR_NOT_FOUND)
 	{
 		DXGI_ADAPTER_DESC desc;
 		adapter->GetDesc(&desc);
@@ -105,62 +120,227 @@ void D3D::LogOutputDisplayModes(IDXGIOutput* output, DXGI_FORMAT format)
 	}
 }
 
+#pragma endregion
+
 void D3D::CreateD3DDevice()
 {
-	DX::ThrowIfFailed(CreateDXGIFactory1(IID_PPV_ARGS(&_mdxgiFactory)));
+	DX::ThrowIfFailed(CreateDXGIFactory1(IID_PPV_ARGS(&_dxgiFactory)));
 
 	HRESULT hardwareResult = D3D12CreateDevice(
 		nullptr,
 		D3D_FEATURE_LEVEL_11_0,
-		IID_PPV_ARGS(&_md3dDevice)
+		IID_PPV_ARGS(&_d3dDevice)
 	);
 
 	if (FAILED(hardwareResult))
 	{
-		DX::ThrowIfFailed(_mdxgiFactory->EnumAdapters(0, &_pWarpAdapter));
+		DX::ThrowIfFailed(_dxgiFactory->EnumAdapters(0, &_warpAdapter));
 
 		DX::ThrowIfFailed(D3D12CreateDevice(
-			_pWarpAdapter.Get(),
+			_warpAdapter.Get(),
 			D3D_FEATURE_LEVEL_11_0,
-			IID_PPV_ARGS(&_md3dDevice)
+			IID_PPV_ARGS(&_d3dDevice)
 		));
 	}
-
-	CreateFence();
 }
 
 void D3D::CreateFence()
 {
-	DX::ThrowIfFailed(_md3dDevice->CreateFence(
+	DX::ThrowIfFailed(_d3dDevice->CreateFence(
 		0,
 		D3D12_FENCE_FLAG_NONE,
-		IID_PPV_ARGS(&_mFence)
+		IID_PPV_ARGS(&_fence)
 	));
 
-
+	_rtvDescriptorSize = _d3dDevice->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+	_dsvDescriptorSize = _d3dDevice->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_DSV);
+	_cbvDescriptorSize = _d3dDevice->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV);
 }
 
-void D3D::CreateCommandList()
+void D3D::MSAAQualityCheck()
+{
+	D3D12_FEATURE_DATA_MULTISAMPLE_QUALITY_LEVELS msQualityLevels;
+	msQualityLevels.Format = _backBufferFormat;
+	msQualityLevels.SampleCount = 4;
+	msQualityLevels.Flags = D3D12_MULTISAMPLE_QUALITY_LEVELS_FLAG_NONE;
+	msQualityLevels.NumQualityLevels = 0;
+	DX::ThrowIfFailed(_d3dDevice->CheckFeatureSupport(
+		D3D12_FEATURE_MULTISAMPLE_QUALITY_LEVELS,
+		&msQualityLevels,
+		sizeof(msQualityLevels)
+	));
+
+	_msaaQuality = msQualityLevels.NumQualityLevels;
+	assert(_msaaQuality > 0 && "Unexpected MSAA quality level.");
+}
+
+void D3D::CreateCommand()
 {
 	D3D12_COMMAND_QUEUE_DESC desc = {};
 	desc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
 	desc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
-	DX::ThrowIfFailed(_md3dDevice->CreateCommandQueue(
-		&desc, IID_PPV_ARGS(&_mCommandQueue)
+	
+	DX::ThrowIfFailed(_d3dDevice->CreateCommandQueue(
+		&desc, IID_PPV_ARGS(&_commandQueue)
 	));
 
-	DX::ThrowIfFailed(_md3dDevice->CreateCommandAllocator(
+	DX::ThrowIfFailed(_d3dDevice->CreateCommandAllocator(
 		D3D12_COMMAND_LIST_TYPE_DIRECT,
-		,
-		IID_PPV_ARGS(&_mCommandAllocator)
+		IID_PPV_ARGS(_commandAllocator.GetAddressOf())		
 	));
 
-	DX::ThrowIfFailed(_md3dDevice->CreateCommandList(
+	DX::ThrowIfFailed(_d3dDevice->CreateCommandList(
 		0,
 		D3D12_COMMAND_LIST_TYPE_DIRECT,
-		_mCommandAllocator,
+		_commandAllocator.Get(),
+		nullptr,
+		IID_PPV_ARGS(_commandList.GetAddressOf())
+	));
 
-	))
+}
+
+void D3D::CreateSwapChain()
+{
+	_swapChain.Reset();
+
+	DXGI_SWAP_CHAIN_DESC desc;
+	desc.BufferDesc.Width = Singleton<Window>()->GetDesc().Width;
+	desc.BufferDesc.Height = Singleton<Window>()->GetDesc().Height;
+	desc.BufferDesc.RefreshRate.Numerator = Singleton<Window>()->GetDesc().MaxFPS;
+	desc.BufferDesc.RefreshRate.Denominator = 1;
+	desc.BufferDesc.Format = _backBufferFormat;
+	desc.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+	desc.SampleDesc.Count = _msaaQuality ? 4 : 1;
+	desc.SampleDesc.Quality = _msaaQuality ? (_msaaQuality - 1) : 0;
+	desc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	desc.BufferCount = SwapChainBufferCount;
+	desc.OutputWindow = Singleton<Window>()->GetDesc().ghMainWnd;
+	desc.Windowed = true;
+	desc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+	desc.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
+
+	DX::ThrowIfFailed(_dxgiFactory->CreateSwapChain(
+		_commandQueue.Get(),
+		&desc,
+		_swapChain.GetAddressOf()
+	));
+}
+
+void D3D::CreateDescriptorHeap()
+{
+	{
+		//rtv
+		D3D12_DESCRIPTOR_HEAP_DESC desc;
+		desc.NumDescriptors = SwapChainBufferCount;
+		desc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+		desc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+		desc.NodeMask = 0;
+		DX::ThrowIfFailed(_d3dDevice->CreateDescriptorHeap(
+			&desc,
+			IID_PPV_ARGS(_rtvHeap.GetAddressOf())
+		));
+	}
+
+	{
+		//dsv
+		D3D12_DESCRIPTOR_HEAP_DESC desc;
+		desc.NumDescriptors = SwapChainBufferCount;
+		desc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_DSV;
+		desc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+		desc.NodeMask = 0;
+		DX::ThrowIfFailed(_d3dDevice->CreateDescriptorHeap(
+			&desc,
+			IID_PPV_ARGS(_dsvHeap.GetAddressOf())
+		));
+	}
+
+}
+
+void D3D::CreateRenderTargetView()
+{
+	CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHeapHandle(
+		_rtvHeap->GetCPUDescriptorHandleForHeapStart()
+	);
+	for (UINT i = 0; i < SwapChainBufferCount; i++)
+	{
+		DX::ThrowIfFailed(_swapChain->GetBuffer(
+			i, IID_PPV_ARGS(&_swapChainBuffer[i])
+		));
+
+		_d3dDevice->CreateRenderTargetView(_swapChainBuffer[i].Get(),
+			nullptr, rtvHeapHandle);
+
+		rtvHeapHandle.Offset(1, _rtvDescriptorSize);
+	}
+}
+
+void D3D::CreateDepthStencilView()
+{
+	D3D12_RESOURCE_DESC desc;
+	desc.Dimension = D3D12_RESOURCE_DIMENSION_TEXTURE2D;
+	desc.Alignment = 0;
+	desc.Width = Singleton<Window>()->GetDesc().Width;
+	desc.Height = Singleton<Window>()->GetDesc().Height;
+	desc.DepthOrArraySize = 1;
+	desc.MipLevels = 1;
+	desc.Format = _depthStencilFormat;
+	desc.SampleDesc.Count = _msaaQuality ? 4 : 1;
+	desc.SampleDesc.Quality = _msaaQuality ? (_msaaQuality - 1) : 0;
+	desc.Layout = D3D12_TEXTURE_LAYOUT_UNKNOWN;
+	desc.Flags = D3D12_RESOURCE_FLAG_ALLOW_DEPTH_STENCIL;
+
+	D3D12_CLEAR_VALUE value;
+	value.Format = _depthStencilFormat;
+	value.DepthStencil.Depth = 1.0f;
+	value.DepthStencil.Stencil = 0;
+	DX::ThrowIfFailed(_d3dDevice->CreateCommittedResource(
+		&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT),
+		D3D12_HEAP_FLAG_NONE,
+		&desc,
+		D3D12_RESOURCE_STATE_COMMON,
+		&value,
+		IID_PPV_ARGS(_depthStencilBuffer.GetAddressOf())
+	));
+
+	CD3DX12_CPU_DESCRIPTOR_HANDLE handle(
+		_dsvHeap->GetCPUDescriptorHandleForHeapStart()
+	);
+
+	_d3dDevice->CreateDepthStencilView(
+		_depthStencilBuffer.Get(),
+		nullptr,
+		handle
+	);
+
+	_commandList->ResourceBarrier(
+		1,
+		&CD3DX12_RESOURCE_BARRIER::Transition(
+			_depthStencilBuffer.Get(),
+			D3D12_RESOURCE_STATE_COMMON,
+			D3D12_RESOURCE_STATE_DEPTH_WRITE
+		)
+	);
+}
+
+void D3D::SetViewport()
+{
+	D3D12_VIEWPORT viewport;
+	viewport.TopLeftX = 0.0f;
+	viewport.TopLeftY = 0.0f;
+	viewport.Width = static_cast<float>(Singleton<Window>()->GetDesc().Width);
+	viewport.Height = static_cast<float>(Singleton<Window>()->GetDesc().Height);
+	viewport.MinDepth = 0.0f;
+	viewport.MaxDepth = 1.0f;
+
+	_commandList->RSSetViewports(1, &viewport);
+}
+
+void D3D::SetScissorRectangle()
+{
+	_scissorRect = { 0, 0, 
+		Singleton<Window>()->GetDesc().Width / 2, 
+		Singleton<Window>()->GetDesc().Height / 2 };
+	_commandList->RSSetScissorRects(1, &_scissorRect);
 }
 
 void D3D::FlushCommandQueue()
@@ -168,6 +348,18 @@ void D3D::FlushCommandQueue()
 	UINT currentFence = 0;
 
 	currentFence++;
+	//DX::ThrowIfFailed();
+}
 
-	DX::ThrowIfFailed();
+D3D12_CPU_DESCRIPTOR_HANDLE D3D::GetCurrentBackBufferView() const
+{
+	
+	return CD3DX12_CPU_DESCRIPTOR_HANDLE(_rtvHeap->GetCPUDescriptorHandleForHeapStart(),
+		_currentBackBuffer,
+		_rtvDescriptorSize);
+}
+
+D3D12_CPU_DESCRIPTOR_HANDLE D3D::GetDepthStencilView() const
+{
+	return _dsvHeap->GetCPUDescriptorHandleForHeapStart();
 }
--- DirectX12Study/D3D.h
+++ DirectX12Study/D3D.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Singletons.h"
 
+const UINT SwapChainBufferCount = 2;
 class D3D :
     public Singletons
 {
@@ -22,18 +23,49 @@ public:
 private:
     void CreateD3DDevice();
     void CreateFence();
-    
-    void CreateCommandList();
+    void MSAAQualityCheck();
+    void CreateCommand();
+    void CreateSwapChain();
+    void CreateDescriptorHeap();
+    void CreateRenderTargetView();
+    void CreateDepthStencilView();
+    void SetViewport();
+    void SetScissorRectangle();
+
     void FlushCommandQueue();
+
+    D3D12_CPU_DESCRIPTOR_HANDLE GetCurrentBackBufferView() const;
+    D3D12_CPU_DESCRIPTOR_HANDLE GetDepthStencilView() const;
+
 private:
     wrl::ComPtr<ID3D12Debug> _debugController;
-    wrl::ComPtr<IDXGIFactory> _mdxgiFactory;
-    wrl::ComPtr<IDXGIAdapter> _pWarpAdapter;
-    wrl::ComPtr<ID3D12Device> _md3dDevice;
-    wrl::ComPtr<ID3D12Fence> _mFence;
+    
+    wrl::ComPtr<IDXGIFactory> _dxgiFactory;
+    wrl::ComPtr<IDXGIAdapter> _warpAdapter;
+    wrl::ComPtr<ID3D12Device> _d3dDevice;
+    wrl::ComPtr<ID3D12Fence> _fence;
+    wrl::ComPtr<IDXGISwapChain> _swapChain;
+    wrl::ComPtr<ID3D12Resource> _swapChainBuffer[SwapChainBufferCount];
 
-    wrl::ComPtr<ID3D12CommandAllocator> _mCommandAllocator;
-    wrl::ComPtr<ID3D12CommandQueue> _mCommandQueue;
-    wrl::ComPtr<ID3D12CommandList> _mCommandList;
+    wrl::ComPtr<ID3D12DescriptorHeap> _rtvHeap;
+    wrl::ComPtr<ID3D12DescriptorHeap> _dsvHeap;
+
+    wrl::ComPtr<ID3D12CommandAllocator> _commandAllocator;
+    wrl::ComPtr<ID3D12CommandQueue> _commandQueue;
+    wrl::ComPtr<ID3D12GraphicsCommandList> _commandList;
+
+    wrl::ComPtr<ID3D12Resource> _depthStencilBuffer;
+
+    UINT _rtvDescriptorSize;
+    UINT _dsvDescriptorSize;
+    UINT _cbvDescriptorSize;
+    UINT _msaaQuality;
+    //UINT _swapChainBufferCount;
+    UINT _currentBackBuffer;
+
+    DXGI_FORMAT _backBufferFormat;
+    DXGI_FORMAT _depthStencilFormat;
+
+    tagRECT _scissorRect;
 };
 
